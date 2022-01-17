77dd74b13ff2fe18ea364b3321db9131f10982a5
ugocode <obiohaugo5@gmail.com>
2022-01-17 10:30:04 +0100

    added a little design in the cartscreen

--- shopify_app/lib/screens/cart_screen.dart
+++ shopify_app/lib/screens/cart_screen.dart
@@ -13,15 +13,56 @@ class _CartScreenState extends State<CartScreen> {
     return Scaffold(
       body: SafeArea(
         child: ListView(
-          children: const [
-            Center(
-                child: Text(
-              "Your Cart",
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.pink),
-            )),
+          children: [
+            const Center(
+              child: Text(
+                "Your Cart",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.pink),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                elevation: 2,
+                shadowColor: Colors.grey,
+                // shape: const ShapeBorder(),
+                child: Container(
+                  height: 150,
+                  width: 250,
+                  decoration: BoxDecoration(
+                      color: Colors.pink,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Image(
+                        image: AssetImage("assets/images/logo.png"),
+                        height: 100,
+                        width: 100,
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [const Text("mableline"), Container()],
+                      ),
+                      const SizedBox(
+                        width: 80,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("N20"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
