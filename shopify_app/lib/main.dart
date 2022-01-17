053c44d1468cf33d3d3d01b70ca88f73a3e126f5
ugocode <obiohaugo5@gmail.com>
2022-01-17 10:28:57 +0100

    renamed the app

--- shopify_app/lib/main.dart
+++ shopify_app/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'ShoppyGirl App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
